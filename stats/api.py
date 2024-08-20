b625c07bd78ce7c194923453f94da89363576f68
ashish-greycube <mr.ashish.shah@gmail.com>
2024-08-20 09:51:09 +0530

    feat: get supplier contact

--- stats/api.py
+++ stats/api.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe import _
 
 @frappe.whitelist()
 @frappe.validate_and_sanitize_search_inputs
@@ -8,3 +9,28 @@ def get_main_department(doctype, txt, searchfield, start, page_len, filters):
 		unique = tuple(set(department_list))
 		# print(unique, '----------ab')
 		return unique
+
+
+@frappe.whitelist()
+def get_supplier_contact(supplier):
+	supplier_contact = frappe.db.sql(
+		"""
+		SELECT
+			contact.name
+		FROM
+			`tabDynamic Link` AS link
+		JOIN
+			`tabContact` AS contact
+		ON
+			contact.name=link.parent
+		WHERE
+			link.link_doctype='Supplier'
+			and link.link_name=%s		
+		ORDER BY
+			contact.creation desc
+		limit 1
+		""",supplier,as_dict=1,debug=1)	
+	if len(supplier_contact) == 0 :
+		frappe.msgprint(_("No contact found for supplier: {0}").format(supplier))
+		return
+	return supplier_contact[0].name
