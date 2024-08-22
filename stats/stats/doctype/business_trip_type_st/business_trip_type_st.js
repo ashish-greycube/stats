8b81396b16e3dd1f486dfbbadb0a34402ab4c62e
durve-greycube <zelam@greycube.in>
2024-08-22 09:56:03 +0530

    minor:create BTR Doctype

---
+++ stats/stats/doctype/business_trip_type_st/business_trip_type_st.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, GreyCube Technologies and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Business Trip Type ST", {
+// 	refresh(frm) {
+
+// 	},
+// });
