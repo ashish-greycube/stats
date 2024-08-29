8138187dfe05222f1baab14631f0195bd0f67d36
Sakshi-Greycube <sakshi@greycube.in>
2024-08-29 17:39:48 +0530

    feat: fetch department budget request account

--- stats/stats/doctype/accumulative_budget_st/accumulative_budget_st.js
+++ stats/stats/doctype/accumulative_budget_st/accumulative_budget_st.js
@@ -1,8 +1,33 @@
 // Copyright (c) 2024, GreyCube Technologies and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Accumulative Budget ST", {
-// 	refresh(frm) {
+frappe.ui.form.on("Accumulative Budget ST", {
+	department_budget_requests(frm){
+        frm.set_value("account_details", "");
+        frm.call({
+            doc: frm.doc,
+            method: "get_department_budget_requests",
+            freeze: true,
+            callback: (r) => {
+                if (r.message) {
+                    // console.log(r.message,'--------msg')
+                    r.message.forEach((e) => {
+                        // console.log(e)
+                        // console.log(e, '--------e')
+                        var d = frm.add_child("account_details");
+                        frappe.model.set_value(d.doctype, d.name, "account_name", e.account_name)
+                        frappe.model.set_value(d.doctype, d.name, "requested_amount", e.requested_amount)
+                    });
+                    refresh_field("account_details");
+                    frm.save()
+                }
+            },
+        });
+    }
+});
 
-// 	},
-// });
+// frappe.ui.form.on("Accumulative Budget ST", {
+//     approved_amount(frm){
+//         let row = locals[cdt][cdn]
+//     }
+// })
--- stats/stats/doctype/accumulative_budget_st/accumulative_budget_st.py
+++ stats/stats/doctype/accumulative_budget_st/accumulative_budget_st.py
@@ -1,9 +1,17 @@
 # Copyright (c) 2024, GreyCube Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class AccumulativeBudgetST(Document):
-	pass
+	@frappe.whitelist()
+	def get_department_budget_requests(self):
+		# print('get_department_budget_requests----')
+		fetch_accumulative_budget_request = frappe.db.sql("""SELECT	ad.account_name, sum(ad.requested_amount) as requested_amount
+													FROM `tabAccounts Details ST` AS ad 
+													inner join `tabDepartment Budget ST` AS db on db.name = ad.parent 
+													where db.fiscal_year = %s group by ad.account_name""",self.fiscal_year,as_dict=True)
+		# print(fetch_accumulative_budget_request, '---fetch_accumulative_budget_request')	
+		return fetch_accumulative_budget_request
