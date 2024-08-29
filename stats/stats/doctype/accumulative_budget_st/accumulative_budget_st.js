48ab3c226293822a4aac0f447fd4d66a7fc418a7
Sakshi-Greycube <sakshi@greycube.in>
2024-08-29 18:11:55 +0530

    feat: department wise budget allocation details

--- stats/stats/doctype/accumulative_budget_st/accumulative_budget_st.js
+++ stats/stats/doctype/accumulative_budget_st/accumulative_budget_st.js
@@ -23,11 +23,36 @@ frappe.ui.form.on("Accumulative Budget ST", {
                 }
             },
         });
+    },
+    allocate_department_wise_budget(frm){
+        frm.set_value("department_wise_budget_allocation_details", "")
+        frm.call({
+            doc: frm.doc,
+            method: "get_department_wise_budegt_allocation",
+            freeze: true,
+            callback: (r) => {
+                if (r.message) {
+                    console.log(r.message,'--------message')
+                    // r.message.forEach((e) => {
+                        // console.log(e)
+                        // console.log(e, '--------e')
+                        // var d = frm.add_child("department_wise_budget_allocation_details");
+                        // frappe.model.set_value(d.doctype, d.name, "main_department", e.account_name)
+                        // frappe.model.set_value(d.doctype, d.name, "budget_account", e.requested_amount)
+                        // frappe.model.set_value(d.doctype, d.name, "requested_amount", e.requested_amount)
+                    // });
+                    refresh_field("department_wise_budget_allocation_details");
+                    frm.save()
+                }
+            },
+        });
     }
 });
 
-// frappe.ui.form.on("Accumulative Budget ST", {
-//     approved_amount(frm){
-//         let row = locals[cdt][cdn]
-//     }
-// })
+frappe.ui.form.on("Accumulative Budget Details ST", {
+    approved_amount(frm, cdt, cdn){
+        let row = locals[cdt][cdn]
+        let difference = row.requested_amount - row.approved_amount
+        frappe.model.set_value(cdt, cdn, "difference", difference)
+    }
+})
--- stats/stats/doctype/accumulative_budget_st/accumulative_budget_st.py
+++ stats/stats/doctype/accumulative_budget_st/accumulative_budget_st.py
@@ -6,12 +6,23 @@ from frappe.model.document import Document
 
 
 class AccumulativeBudgetST(Document):
+	def validate(self):
+			self.calculate_amount_difference()
+
+	def calculate_amount_difference(self):
+		if len(self.account_details) > 0:
+			for row in self.account_details:
+				row.difference = (row.requested_amount or 0) - (row.approved_amount or 0)
+
 	@frappe.whitelist()
 	def get_department_budget_requests(self):
-		# print('get_department_budget_requests----')
 		fetch_accumulative_budget_request = frappe.db.sql("""SELECT	ad.account_name, sum(ad.requested_amount) as requested_amount
 													FROM `tabAccounts Details ST` AS ad 
 													inner join `tabDepartment Budget ST` AS db on db.name = ad.parent 
 													where db.fiscal_year = %s group by ad.account_name""",self.fiscal_year,as_dict=True)
-		# print(fetch_accumulative_budget_request, '---fetch_accumulative_budget_request')	
 		return fetch_accumulative_budget_request
+	
+	@frappe.whitelist()
+	def get_department_wise_budegt_allocation(self):
+		list_a = ["Hello", "World"]
+		return list_a
