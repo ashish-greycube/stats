51fb0950314410f81fcd1eeb4386b13c0054f557
Sakshi-Greycube <sakshi@greycube.in>
2024-09-04 13:29:44 +0530

    feat: set distinct filter

--- stats/api.py
+++ stats/api.py
@@ -7,7 +7,7 @@ from dateutil import relativedelta
 @frappe.validate_and_sanitize_search_inputs
 def get_main_department(doctype, txt, searchfield, start, page_len, filters):
 		
-		department_list = frappe.get_all("Department", filters={"is_group":0}, fields=["parent_department"], as_list=1)
+		department_list = frappe.get_all("Department", filters={"is_group":0}, fields=["distinct parent_department"], as_list=1)
 		unique = tuple(set(department_list))
 		# print(unique, '----------ab')
 		return unique
