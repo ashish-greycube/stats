d6406af946039c8cf8d98669e64113505bd6e807
Sakshi-Greycube <sakshi@greycube.in>
2024-11-14 13:46:40 +0530

    validate: resignation date

--- stats/stats/doctype/employee_resignation_st/employee_resignation_st.py
+++ stats/stats/doctype/employee_resignation_st/employee_resignation_st.py
@@ -1,9 +1,23 @@
 # Copyright (c) 2024, GreyCube Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
-
+from frappe.utils import getdate, nowdate
 
 class EmployeeResignationST(Document):
-	pass
+	def validate(self):
+		self.validate_resignation_date()
+
+	def validate_resignation_date(self):
+		if self.resignation_date:
+			today_month = getdate(nowdate()).month
+			resignation_month = getdate(self.resignation_date).month
+
+			if today_month == resignation_month:
+				# today_date = nowdate().day
+				resignation_date = getdate(self.resignation_date).day
+
+				if resignation_date >= 11:
+					frappe.throw(_("For this Month, Payroll is generated so you cann't apply for resignation"))
