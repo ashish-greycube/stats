0d76dd79d8d2fccc487e6757c16e961ef683ae5d
Sakshi-Greycube <sakshi@greycube.in>
2025-01-03 13:18:08 +0530

    fix: fetch correct availabel buget without cost center

--- stats/budget.py
+++ stats/budget.py
@@ -13,11 +13,11 @@ def get_budget_account_details(budget_cost_center,budget_expense_account,fiscal_
         """
             select
                             b.cost_center,
-                            gl.account,
+                            ba.account,
                             ba.budget_amount as approved,
                             IF(sum(gl.debit), sum(gl.debit)-sum(gl.credit),0) as used,
                             IF(sum(gl.debit),ba.budget_amount-sum(gl.debit)-sum(gl.credit),ba.budget_amount) as available, 
-                            gl.fiscal_year
+                            b.fiscal_year
             from
                             `tabBudget` b
                             inner join 
@@ -47,23 +47,24 @@ def get_budget_account_details_without_cost_center(budget_expense_account,fiscal
         }
     budget_details = frappe.db.sql(
         """
-            select
-                            b.cost_center,
-                            gl.account,
-                            ba.budget_amount as approved,
-                            IF(sum(gl.debit), sum(gl.debit)-sum(gl.credit),0) as used,
-                            IF(sum(gl.debit),ba.budget_amount-sum(gl.debit)-sum(gl.credit),ba.budget_amount) as available, 
-                            gl.fiscal_year
-            from
-                            `tabBudget` b
-                            inner join 
-                            `tabBudget Account` ba
-                            on  b.name = ba.parent and b.docstatus = 1
-                            left join    `tabGL Entry` gl
-                            on ba.account = gl.account 
-                            and b.cost_center = gl.cost_center 
-                            and gl.fiscal_year = %(fiscal_year)s
-                            and gl.account = %(budget_expense_account)s     
+        select
+            gl.account,
+            ba.budget_amount as approved,
+            IF(sum(gl.debit), sum(gl.debit)-sum(gl.credit),0) as used,
+            IF(sum(gl.debit),ba.budget_amount-sum(gl.debit)-sum(gl.credit),ba.budget_amount) as available, 
+            b.fiscal_year
+        from
+            `tabBudget` b
+            inner join 
+            `tabBudget Account` ba
+            on  b.name = ba.parent and b.docstatus = 1
+            left join `tabGL Entry` gl
+            on ba.account = gl.account 
+            and b.cost_center = gl.cost_center 
+            and gl.fiscal_year = b.fiscal_year
+            and gl.account = ba.account
+        WHERE
+            ba.account = %(budget_expense_account)s and b.fiscal_year = %(fiscal_year)s
             group by
                 gl.name
         """,filters,as_dict=1,debug=1)
